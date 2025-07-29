5c0b2c493e0d2d1c05083db3b8f1b95a291c852d
O deng <oms9623@naver.com>
2025-07-29 13:39:32 +0900

    20250729(1)

--- HelloJSP/src/main/java/com/yedam/Main.java
+++ HelloJSP/src/main/java/com/yedam/Main.java
@@ -1,41 +1,49 @@
 package com.yedam;
 
-import org.apache.ibatis.session.SqlSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.yedam.common.DBUtil;
-import com.yedam.mapper.BoardMapper;
-import com.yedam.service.BoardService;
-import com.yedam.service.BoardServiceImpl;
-import com.yedam.vo.BoardVO;
+interface Controller {
+	public void exe();
+}
+
+class Sample implements Controller {
+
+	@Override
+	public void exe() {
+		System.out.println("Sample 클래스");
+	}
+	
+}
 
 public class Main {
 	public static void main(String[] args) {
 		
-		BoardService svc = new BoardServiceImpl(); // 업무기능
+		ArrayList<Book> list = new ArrayList<>();
+//		Book book = list.get(0);
 		
+		// 키=값
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("홍길동", 80);
+		map.put("박길동", 70);
 		
-		//위에 업무기능 때문에 빠진다
-//		SqlSession sqlSession = DBUtil.getInstance().openSession();
-//		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		Integer result = map.get("홍길동");
+		System.out.println(result);
 		
-		//전체조회
-//		List<BoardVO> list = mapper.selectList();
-//		for(BoardVO board : list) {
-//			System.out.println(board.toString());
-//		}
-		
-		//단건조회
-		int searchNo = 1;
-//		BoardVO board = mapper.selectBoard(searchNo);
-//		System.out.println(board.toString());
-		
-		//조회,조회수 증가 => 업무(조회: 글번호조회 + 조회카운트)
-//		BoardVO board = mapper.selectBoard(searchNo);
-//		mapper.updateCount(searchNo);
-		
-		BoardVO board = svc.searchBoard(searchNo);
-		
-		System.out.println(board.toString());
+		Map<String, Controller> controls = new HashMap<>();
+		controls.put("abc.do", new Sample());
+		controls.put("ddc.do", new Controller() {
 
+			@Override
+			public void exe() {
+				System.out.println("Test 입니다");
+			}
+		
+		});
+		
+		Controller val = controls.get("Sample");
+		val.exe();
+		
 	}//end main
 }//end class
